c90232529ebd7d064d2938c3d4fe13ed23f42ff7
southpaw1371 <southpaw1371@gmail.com>
2025-05-20 18:15:11 -0500

    callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,17 +1,14 @@
-// api/callback.js
 export default function handler(req, res) {
   const query = req.query;
 
-  if (query.code) {
-    console.log("Authorization Code:", query.code);
-  }
+  console.log("eBay OAuth callback received:", query);
 
   res.status(200).send(`
     <html>
-      <body style="font-family: sans-serif; text-align: center; margin-top: 50px;">
-        <h1>eBay Authorization Successful</h1>
-        <p>You can now close this window.</p>
-        <code>${JSON.stringify(query)}</code>
+      <body style="font-family: sans-serif; padding: 2rem;">
+        <h2>✅ eBay OAuth Callback Received</h2>
+        <p>Here is your authorization code and state:</p>
+        <pre>${JSON.stringify(query, null, 2)}</pre>
       </body>
     </html>
   `);
